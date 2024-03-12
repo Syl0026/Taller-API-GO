bb4a12a90c969047673ff98ebd5974672e6f36b3
Sylvana Salinas <syl0026.ssp@gmail.com>
2024-03-12 15:27:57 -0600

    Agregar validación de longitud

--- Services/Validators/AlumnValidator.go
+++ Services/Validators/AlumnValidator.go
@@ -8,6 +8,8 @@ import "tallerAPIgo/Models" //!Se importa solo
 //& func es una función
 //$ func NombreFunción(variable *Tipo(Clase).Variable(estructura)) (tipoReturn){}
 func CreateAlumnValidator(model *Models.AlumnModel) (bool, string) {
+
+	//% Estamos validando datos requeridos
 	if model.Name == "" {
 		return false, "Error: Nombre no válido, el nombre es requerido"
 	}
@@ -23,6 +25,23 @@ func CreateAlumnValidator(model *Models.AlumnModel) (bool, string) {
 	}
 	if len(model.Phone) == 0 { //& len indica la longitud de la variable
 		return false, "Error, Teléfono inválido, el teléfono es requerido"
+	}
+
+	//% Verificacion de longitud de campos
+	if len(model.Name) > 150 {
+		return false, "Error: Logitud máxima es 150"
+	}
+	if len(model.LastName) > 150 {
+		return false, "Error: Logitud máxima es 150"
+	}
+	if len(model.Curp) > 18 {
+		return false, "Error: Logitud máxima es 18"
+	}
+	if len(model.Email) > 200 {
+		return false, "Error: Logitud máxima es 200"
+	}
+	if len(model.Phone) > 50 {
+		return false, "Error: Logitud máxima es 50"
 	}
 
 	return true, "" //$ Valores de return
