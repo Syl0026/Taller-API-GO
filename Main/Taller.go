f1643c9802aad0a6e02ce03f351c50bf94a1b366
Sylvana Salinas <syl0026.ssp@gmail.com>
2024-03-12 17:33:02 -0600

    Edición de Taller go

--- Main/Taller.go
+++ Main/Taller.go
@@ -1,8 +1,27 @@
 package main //! SIEMPRE SE DEBE DE ESCRIBIR EL PAQUETE O CARPETA
 
-import "fmt"
+import (
+	"fmt"
+	"tallerAPIgo/Models"
+	"tallerAPIgo/Services/Services"
+)
 
 //% Ejemplo de función
+// func main() {
+// 	fmt.Println("Hola mundo")
+// }
+
 func main() {
-	fmt.Println("Hola mundo")
+	model := Models.AlumnModel{
+		Name:     "Juanito",
+		LastName: "Perez",
+		Curp:     "00000000000000000",
+		Email:    "juanpg@gmail.com",
+		Phone:    "9999999999",
+	}
+	Services.CreateAlumn(&model)
+
+	result, msg := Services.CreateAlumn(&model)
+	fmt.Println(result)
+	fmt.Println(msg)
 }
